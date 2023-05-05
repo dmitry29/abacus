326a7903a57a457e3e2a9cce8df219bf194b9e28
dmitry29 <dmitry29push@gmail.com>
2023-05-05 23:21:05 +0300

    com1

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,1 @@
 import "https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js";
-
-var frame = document.createElement("iframe")
-frame.onload = function() {
-  var frameWin = frame.contentWindow
-  console.log(frameWin.location.origin) // "null"
-  console.log(frameWin.origin) // "https://stargate.example"
-}
-document.body.appendChild(frame)
